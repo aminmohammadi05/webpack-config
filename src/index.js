5ba004fff909769bcab9ff2ec64c1424ef6f58d6
Amin Mohammadi <amin_mohammadi05@yahoo.com>
2022-08-30 10:55:38 +0430

    add css sass postcss

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
+import "./styles/index.scss";
 const elvenShieldRecipe = {
     leatherStrips: 2,
     ironIngot: 1,
     refineMoonstone: 4
 };
+const elvenGountletsRecipe = {
+    ...elvenShieldRecipe,
+    leather: 1,
+    refinedMoonstone: 4
+}
 
 console.log(elvenShieldRecipe);
+console.log(elvenGountletsRecipe);
